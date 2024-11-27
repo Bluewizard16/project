aab5075181c883e35a0a3864f4c16306e441355c
Querry Hurtado <williamquerry2@gmail.com>
2024-11-27 00:26:23 -0500

    agregué componentes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>
-        Welcome to the homepage
-    </h1>
+    <div class="max-w-4xl mx-auto px-4">
+        <x-alert2 type="info" class="mb-4">
+            <x-slot name="alertType">
+                Info alert
+            </x-slot>
+            Contenido de la alerta
+        </x-alert2>
+        <p>Hola mundo</p>
+    </div>
 </body>
 </html>
